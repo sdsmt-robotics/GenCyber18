05dae1a78a9e53f97f63f33c53aa49f92c23fda9
7103344 <remington.bullis@mines.sdsmt.edu>
2018-06-27 12:33:36 -0600

    Retry #2

---
+++ day3/ir_sensor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import geekbot
+from time import sleep as wait
+
+geek = geekbot.Robot(57600)
+if geek.is_connected() == False:
+    exit()
+
+#####################################
+
+MIN_DIST = 20 # Change this number to detect closer ojbects 
+
+while 1:
+    
+    front_dist = geek.get_ir_distance() 
+    print(front_dist)
+
+    if dist <= MIN_DIST:
+            geek.lights_on()
+            geek.beep(1)
+            geek.lights_off()
+            geek.set_ir_position(0)
+            wait(.2)               
+            left_dist = geek.get_ir_distance()
+
+            if left_dist <= MIN_DIST:
+                geek.set_ir_position(90)    
+                geek.turn_right(50, 0.75)   # Change the speed and time to turn 90* 
+                                        
+            else:
+                geek.set_ir_position(90)
+                geek.turn_left(50, 0.75)    # Change the speed and time to turn 90* 
+    wait(.2)
+
+##################################
+geek.shutdown()
+
